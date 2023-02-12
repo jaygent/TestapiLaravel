b8c471db5423f9ca2488de4192a5a0e9340d1a97
jaygent <jaygenf@gmail.com>
2023-02-13 01:18:20 +0300

    -init

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::middleware('auth:sanctum')->prefix('/v1')->group(function (){
+Route::prefix('/v1')->group(function (){
     Route::apiResource('/products',\App\Http\Controllers\Products::class);
     Route::apiResource('/product',\App\Http\Controllers\Product::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $user=User::find(4)->createToken('test');
-    return $user->plainTextToken;
+    return view('welcome');
 });
